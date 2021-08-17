70ad144ef413b82bf781eb6947e9750c084124e2
Damerian Dark <newbmailz@gmail.com>
2021-08-17 10:20:48 -0500

    have the correct parameters in now and was able to recieve a response.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,27 @@
+// var publicKey = "cf666bb77e15be90b02e3679ffa84cf0";
+// var privateKey = "d50814d942d89079bc056b8c2cb93425a4258ede";
+// var marvelCharacterUrl = `http://gateway.marvel.com/v1/public/comics?apikey=${publicKey}`;
+
+
+// fetch(marvelCharacterUrl)
+//     .then(async function (response) {
+//         if(response.ok){
+//         const marvelCharacterData = await response.json();
+//             console.log(marvelCharacterData);
+//     }  else {
+//         response.text().then(marvelCharacterData => {
+//             console.log(marvelCharacterData);
+//         })
+//     }
+// });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let searchQuery = 'Cyclops';
 let ytApiKey = "AIzaSyDiOZ44nzVN6XsP85hiAU76fyZkNyw7hN8";
-let ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?key=${ytApiKey}`;
+// let ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?key=${ytApiKey}`;
+let ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?q=${searchQuery}&part=id,topicDetails,contentDetails&chart=mostPopular&key=${ytApiKey}`;
 fetch(ytUrl)
 .then(function (response) {
     if (response.ok) {
