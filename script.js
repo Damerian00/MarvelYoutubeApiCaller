29f32e1801d6554c51b40eb24f6ab015b374a430
Tina Pham <tina.pham8912@gmail.com>
2021-08-17 07:14:31 -0700

    marvel api

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var publicKey = "cf666bb77e15be90b02e3679ffa84cf0";
 var privateKey = "d50814d942d89079bc056b8c2cb93425a4258ede";
-var marvelCharacterUrl = `http://gateway.marvel.com/v1/public/comics?apikey=${marvelCharacterAPIKEY}`;
+var marvelCharacterUrl = `http://gateway.marvel.com/v1/public/comics?apikey=${publicKey}`;
 
 
 fetch(marvelCharacterUrl)
-    .then(function (response) {
+    .then(async function (response) {
         if(response.ok){
-        return response.json().then(marvelCharacterData => {
+        const marvelCharacterData = await response.json();
             console.log(marvelCharacterData);
-        })
     }  else {
         response.text().then(marvelCharacterData => {
             console.log(marvelCharacterData);
