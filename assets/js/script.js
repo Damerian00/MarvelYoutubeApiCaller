5c0d96abdbf6f1b7ab5e2e64a00d4dac3dc491c2
Tina Pham <tina.pham8912@gmail.com>
2021-08-18 16:07:03 -0700

    script changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,39 @@
 const publicKey = "cf666bb77e15be90b02e3679ffa84cf0";
-var searchBox = "Storm";
-var marvelCharacterUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchBox}&apikey=${publicKey}`;
 
-fetch(marvelCharacterUrl)
+
+function searchAPI () {
+    var searchBox = document.querySelector("#characterInput").value;
+    var marvelCharacterUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchBox}&apikey=${publicKey}`;
+
+    fetch(marvelCharacterUrl)
     .then(async function (response) {
         if(response.ok){
-        const marvelCharacterData = await response.json();
+            const marvelCharacterData = await response.json();
             console.log(marvelCharacterData);
-    }  else {
-        response.text().then(marvelCharacterData => {
+            console.log(marvelCharacterData.data.results[0].thumbnail.path + "/portrait_uncanny.jpg")
+            document.querySelector("#characterImg").setAttribute("src", marvelCharacterData.data.results[0].thumbnail.path + "/portrait_uncanny.jpg") 
+            document.querySelector("#charBio").textContent = marvelCharacterData.data.results[0].description;
+
+            // showCharacterImage(marvel)
+        }  else {
+            response.text().then(marvelCharacterData => {
             console.log(marvelCharacterData);
         })
     }
 });
+}
 
-function showCharacterImage () {
-    document.getElementById("Storm").textContent = results[0].name;
-    console.log(results[0].name);
-    var img = document.createElementby("img");
-    img.src = results[0].thumbnail.path + " .jpg";
-    document.getElementById("pic").appendChild(img);
-
-};
+document.querySelector("#searchCharacter").addEventListener("click", searchAPI)
 
 
-//document.getElementById("search").addEventListener("click", searchBox);
-// let searchQuery = 'Cyclops';
-// let ytApiKey = "AIzaSyDiOZ44nzVN6XsP85hiAU76fyZkNyw7hN8";
-// // let ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?key=${ytApiKey}`;
-// let ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?q=${searchQuery}&part=id,topicDetails,contentDetails&chart=mostPopular&key=${ytApiKey}`;
-// fetch(ytUrl)
-// .then(function (response) {
-//     if (response.ok) {
-       
-//         response.json().then(ytData => {
-//            console.log(ytData);
-            
-//         })
- 
-//     }   else {
-//         response.text().then(ytData => {
-//             console.log(ytData);
-             
-//          })
+// function showCharacterImage (object) {
+//     document.getElementById("Storm").textContent = results[0].name;
+//     console.log(results[0].name);
+//     var img = document.createElementby("img");
+//     img.src = results[0].thumbnail.path + " .jpg";
+//     document.getElementById("pic").appendChild(img);
 
-
+// };
 
 let ytApiKey = "AIzaSyDiOZ44nzVN6XsP85hiAU76fyZkNyw7hN8";
 let ytUrl = `https://youtube.googleapis.com/youtube/v3/search?q=Marvel,${searchBox}&type=video&part=snippet&chart=mostPopular&key=${ytApiKey}`;
