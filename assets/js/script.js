38e83fa64eba4a1eeccdcb7be77c89ce41a38400
Tina Pham <tina.pham8912@gmail.com>
2021-08-19 07:15:42 -0700

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,29 @@
 const publicKey = "cf666bb77e15be90b02e3679ffa84cf0";
-var searchBox = "Storm";
-var marvelCharacterUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchBox}&apikey=${publicKey}`;
+let charVid = document.querySelector('#charVid');
+let charLinks = document.querySelector('#charLinks');
 
-fetch(marvelCharacterUrl)
+function searchAPI () {
+    var searchBox = document.querySelector("#characterInput").value;
+    var marvelCharacterUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchBox}&apikey=${publicKey}`;
+
+    fetch(marvelCharacterUrl)
     .then(async function (response) {
         if(response.ok){
-        const marvelCharacterData = await response.json();
+            const marvelCharacterData = await response.json();
             console.log(marvelCharacterData);
-    }  else {
-        response.text().then(marvelCharacterData => {
+            console.log(marvelCharacterData.data.results[0].thumbnail.path + "/portrait_uncanny.jpg")
+            document.querySelector("#characterImg").setAttribute("src", marvelCharacterData.data.results[0].thumbnail.path + "/portrait_uncanny.jpg") 
+            document.querySelector("#charBio").textContent = marvelCharacterData.data.results[0].description;
+            
+        }  else {
+            response.text().then(marvelCharacterData => {
             console.log(marvelCharacterData);
         })
     }
 });
-
-function showCharacterImage () {
-    document.getElementById("Storm").textContent = results[0].name;
-    console.log(results[0].name);
-    var img = document.createElementby("img");
-    img.src = results[0].thumbnail.path + " .jpg";
-    document.getElementById("pic").appendChild(img);
-
-};
-
-function showCharacterBio () {
-    
 }
 
-
-//document.getElementById("search").addEventListener("click", searchBox);
-// let searchQuery = 'Cyclops';
-// let ytApiKey = "AIzaSyDiOZ44nzVN6XsP85hiAU76fyZkNyw7hN8";
-// // let ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?key=${ytApiKey}`;
-// let ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?q=${searchQuery}&part=id,topicDetails,contentDetails&chart=mostPopular&key=${ytApiKey}`;
-// fetch(ytUrl)
-// .then(function (response) {
-//     if (response.ok) {
-       
-//         response.json().then(ytData => {
-//            console.log(ytData);
-            
-//         })
- 
-//     }   else {
-//         response.text().then(ytData => {
-//             console.log(ytData);
-             
-//   })
+document.querySelector("#searchCharacter").addEventListener("click", searchAPI)
 
 
 let ytApiKey = "AIzaSyDiOZ44nzVN6XsP85hiAU76fyZkNyw7hN8";
@@ -55,12 +32,23 @@ fetch(ytUrl)
 .then(function (response) {
     if (response.ok) {
        
-        response.json().then(ytData => {
+        response.json().then(ytData => {``
            console.log(ytData);
            for (let i = 0; i < ytData.items.length; i++) {
                let video = ytData.items[i].id.videoId
                console.log(video);
                let link =  `https://www.youtube.com/watch?v=${video}`;
+               let vTag = document.createElement('iframe');
+               vTag.classList = "query p-2";
+               vTag.src = `https://www.youtube.com/embed/${video}`;
+               let aTag = document.createElement('a');
+               aTag.classList = "p-2 bg-secondary text-center";
+               charVid.appendChild(vTag);
+               aTag.href = link;
+               aTag.setAttribute("target", "_blank")
+               aTag.innerHTML = searchBox.toUpperCase();
+               charLinks.appendChild(aTag); 
+
                console.log(link);
                
                
@@ -75,16 +63,3 @@ fetch(ytUrl)
     }
 }  )  
 
-  
-  /* 
-
-  let video = '';
-  let videos = document.QuerySelctor('#vids');
-
-  array.forEach(element => {
-      
-  });
-  data.items */
-
-  /*
-  https://www.youtube.com/watch?v=Y1J9_9-vNcU */
